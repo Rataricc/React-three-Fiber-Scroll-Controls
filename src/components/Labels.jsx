f4a11c74979c3b70b2255b84c209543c3e5ab868
Lautaro Rataric <lautarorataric163@gmail.com>
2024-04-10 21:50:22 -0300

    updating components and styles

--- src/components/Labels.jsx
+++ src/components/Labels.jsx
@@ -1,93 +1,30 @@
-/*
 import React from "react";
+import Button from 'react-bootstrap/Button';
 
-const Labels = ({ text, description, button }) => {
-  return (
-    <div className="text-overlay">
-      <h1>{text}</h1>
-      <p>{description}</p>
-      
-    </div>
-  );
+let actions = {
+    "Reserva turnos": 'https://nubiant.com/app/5ee2ef93-0cf3-45d3-a09b-10a95242d1eb/mis_turnos/5e4d98ea-e5cf-43c8-997e-2a53ec36f3dc',
+    "Salud Total": 'https://nubiant.com/saludtotal',
+    "Gestiona tu historial médico": 'https://nubiant.com/app/5ee2ef93-0cf3-45d3-a09b-10a95242d1eb/mis_atenciones/f3167cd9-33ad-4aa4-9b43-69d34bbda1e3',
+    "Tramita tus recetas de medicamentos": 'https://nubiant.com/app/5ee2ef93-0cf3-45d3-a09b-10a95242d1eb/prescripciones/6fb7999e-9fef-42bd-972a-79a46c78731f',
 };
 
-export default Labels;
-*/
-/*
-import React from "react";
-
-export default function Labels() {
-  const cards = [
-    {
-      id: "Classic_Card",
-      title: "Classic",
-      subheader: "Nuka-cola",
-      description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum, itaque deserunt minima nam alias saep quia et nulla architecto voluptatem.`,
-      ml: "350 ml",
-      points: ["extreme", "extreme", "extreme"],
-    },
-    {
-      id: "Quantum_Card",
-      title: "Quantum",
-      subheader: "Nuka-cola",
-      description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum, itaque deserunt minima nam alias saep quia et nulla architecto voluptatem.`,
-      ml: "350 ml",
-      points: ["extreme", "extreme", "extreme"],
-    },
-    {
-      id: "Sunset_Card",
-      title: "Sunset",
-      subheader: "Zarzaparrilla",
-      description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum, itaque deserunt minima nam alias saep quia et nulla architecto voluptatem.`,
-      ml: "350 ml",
-      points: ["extreme", "extreme", "extreme"],
-    },
-  ];
-
-  return (
-    <div className={"labels_container"}>
-      <div className={"labels_wrapper"}>
-        {cards.map((card, index) => (
-          <div
-            id={card.id}
-            key={card.id}
-            className={`label_card label_card--hidden label_${index}`}
-          >
-            <div>
-              <h1 className={"text_header"}>{card.title}</h1>
-              <p className={"text_subheader"}>{card.subheader}</p>
-              <p className={"text_body"}>{card.description}</p>
-            </div>
-            <div>
-              <h1 className={"text_subheader"}>{card.ml}</h1>
-            </div>
-            <div>
-              <ul>
-                {card.points.map((point, index) => (
-                  <li key={index} className="text_item_des">
-                    <p className={"text_body"}>
-                      {point}
-                    </p>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-}
-*/
-
-import React from "react";
-
 export default function Label() {
+
+    const handleButtonClick = (title) => {
+        const url = actions[title];
+        if (url) {
+            window.open(url, '_blank'); // Abre la URL en una nueva pestaña
+        } else {
+            // Acción específica para el botón que no tiene URL
+            console.log(`Acción para ${title}`);
+        }
+    };
+
     const labels = [
         {
             id: "TopLeftLabel",
-            title: "hola",
-            content: "Text for top left corner",
+            title: "Reserva turnos",
+            content: "Programa tus consultas médicas con facilidad.",
         },
         {
             id: "TopRightLabel",
@@ -96,13 +33,13 @@ export default function Label() {
         },
         {
             id: "BottomLeftLabel",
-            title: "Bottom Left",
-            content: "Text for bottom left corner",
+            title: "Gestiona tu historial médico",
+            content: "SaludTotal te ofrece acceso instantáneo a tu historial médico",
         },
         {
             id: "BottomRightLabel",
-            title: "Bottom Right",
-            content: "Text for bottom right corner",
+            title: "Tramita tus recetas de medicamentos",
+            content: "Renueva tus medicamentos fácilmente desde tu perfil en SaludTotal",
         },
     ];
 
@@ -113,6 +50,7 @@ export default function Label() {
                     <div id={label.id} key={label.id} className={"label label_card--hidden"}>
                         <h1 className={"text_title"}>{label.title}</h1>
                         <p className={"text_content"}>{label.content}</p>
+                        <Button onClick={() => handleButtonClick(label.title)} variant="info">{label.title}</Button>
                     </div>
                 ))}
             </div>
--- src/components/Utils.jsx
+++ src/components/Utils.jsx
@@ -1,63 +1,82 @@
 export const GenerateAnimations = (scene) => {
     const Anamations = [
         //page 1
-      {
-        target: scene.getObjectByName("Sketchfab_model").rotation,
-        pointTime: 0,
-        animationsProperties: {
-          y: 0, // Rotación inicial
-          onUpdate: () => {},
+        {
+            target: scene.getObjectByName("Sketchfab_model").rotation,
+            pointTime: 0,
+            animationsProperties: {
+                y: 0, // Rotación inicial
+                onUpdate: () => { },
+            },
         },
-      },
-      {
-        target: document.getElementById("TopRightLabel"), 
-        pointTime:0, 
-        animationsProperties: {
-            opacity: 1, 
-            //left: "25%", 
-            onUpdate: () => {}, 
-        }
-      }, 
+        {
+            target: document.getElementById("TopRightLabel"),
+            pointTime: 1,
+            animationsProperties: {
+                opacity: 1,
+                left: "25%",
+                onUpdate: () => { },
+            }
+        },
         //page 2
-      {
-        target: scene.getObjectByName("Sketchfab_model").rotation,
-        pointTime: 1,
-        animationsProperties: {
-          y: Math.PI  , // Rotación de 90 grados (1/4 de vuelta)
-          onUpdate: () => {},
+        {
+            target: scene.getObjectByName("Sketchfab_model").rotation,
+            pointTime: 1,
+            animationsProperties: {
+                y: Math.PI, // Rotación de 90 grados (1/4 de vuelta)
+                onUpdate: () => { },
+            },
         },
-      },
-      {
-        target: document.getElementById("TopLeftLabel"), 
-        pointTime:1, 
-        animationsProperties: {
-            opacity: 1, 
-            //left: "25%", 
-            onUpdate: () => {}, 
-        }
-      }, 
-      
-      //page 3
-      {
-        target: scene.getObjectByName("Sketchfab_model").rotation,
-        pointTime: 2,
-        animationsProperties: {
-          y: Math.PI, // Rotación de 180 grados (media vuelta)
-          onUpdate: () => {},
+        {
+            target: document.getElementById("TopLeftLabel"),
+            pointTime: 3,
+            animationsProperties: {
+                opacity: 1,
+                //rigth: "25%", 
+
+                onUpdate: () => { },
+            },
         },
-      },
-      
+
+        //page 3
+        {
+            target: scene.getObjectByName("Sketchfab_model").rotation,
+            pointTime: 2,
+            animationsProperties: {
+                y: Math.PI, // Rotación de 180 grados (media vuelta)
+                onUpdate: () => { },
+            },
+        },
+        {
+            target: document.getElementById("BottomLeftLabel"),
+            pointTime: 3,
+            animationsProperties: {
+                opacity: 1,
+                //rigth: "25%", 
+
+                onUpdate: () => { },
+            },
+        },
+
         //page 4
-      {
-        target: scene.getObjectByName("Sketchfab_model").rotation,
-        pointTime: 3,
-        animationsProperties: {
-          //y: (3 * Math.PI) / 2, // Rotación de 270 grados (3/4 de vuelta)
-          onUpdate: () => {},
+        {
+            target: scene.getObjectByName("Sketchfab_model").rotation,
+            pointTime: 3,
+            animationsProperties: {
+                y: (3 * Math.PI) / 2, // Rotación de 270 grados (3/4 de vuelta)
+                onUpdate: () => { },
+            },
         },
-      },
+        {
+            target: document.getElementById("BottomRightLabel"),
+            pointTime: 0,
+            animationsProperties: {
+                opacity: 1,
+                left: "50%",
+
+                onUpdate: () => { },
+            },
+        },
     ];
     return Anamations;
-  };
-  
-
+};
